9ab1f8533c51a525b235a1201e90ad9930609d41
Jack Everson <111256162+JackEverson@users.noreply.github.com>
2026-09-10 12:42:34 +1000

    fixing issue with snacks

--- init.lua
+++ init.lua
@@ -1081,7 +1081,7 @@ do
         { section = 'keys', gap = 1, padding = 1 },
         { pane = 2, icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
         { pane = 2, icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
-        { section = 'startup' },
+        { text = { { 'Neovim ' .. tostring(vim.version()), hl = 'footer' } }, align = 'center' },
       },
     },
   }
