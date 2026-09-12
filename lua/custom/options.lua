c6f741f2a0ceb3456d948d7172d12037deb6d91f
Jack Everson <jack.everson0@gmail.com>
2026-09-12 12:16:11 +1000

    changes for Python coding

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -64,3 +64,8 @@ vim.o.scrolloff = 4
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = -1
