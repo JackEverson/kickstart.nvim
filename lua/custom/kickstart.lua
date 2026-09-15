c9462329f668a425b7611bc471a6fc873fc0634c
Jack Everson <jack.everson0@gmail.com>
2026-09-15 11:45:28 +1000

    automatic Mason servers

--- lua/custom/kickstart.lua
+++ lua/custom/kickstart.lua
@@ -346,7 +346,7 @@ require('mason').setup {}
 
 -- Translates between nvim-lspconfig server names and mason.nvim package names (e.g. lua_ls <-> lua-language-server)
 require('mason-lspconfig').setup {
-  automatic_enable = false, -- Change this to true if you want to automatically enable servers that are installed manually (e.g. via :Mason / :MasonInstall)
+  automatic_enable = true, -- Change this to true if you want to automatically enable servers that are installed manually (e.g. via :Mason / :MasonInstall)
 }
 
 -- You can press `g?` for help in this menu.
